660b7ce86f4c707029f8b363e359a38cdb0e335b
Oleg <oleg.mytnyk@gmail.com>
2017-05-23 22:37:18 +0300

    some fixes

--- Screenshot/src/log.hpp
+++ Screenshot/src/log.hpp
@@ -4,18 +4,20 @@
 #include <time.h>
 
 class Log
-{
-public:
-  void LogF(std::string error)
   {
-    std::ifstream i("log.txt");
-    std::string str((std::istreambuf_iterator<char>(i)), std::istreambuf_iterator<char>());
-    i.close();
-    time_t t = time(0);
-    struct tm * now = localtime(&t);
-    std::string name = std::to_string(now->tm_mday) + "_" + std::to_string(now->tm_hour) + ":" + std::to_string(now->tm_min) + ":" + std::to_string(now->tm_sec) + " - " + error + "\n";
-    std::ofstream o("log.txt");
-    o << str + name;
-    o.close();
-  }
-};
+  private:
+    std::ofstream o;
+  public:
+    Log() : o("ScreenshotLog.txt")
+      {
+
+      }
+    void LogF(const std::string& error)
+      {
+      time_t t = time(0);
+      struct tm * now = localtime(&t);
+      std::string message = std::to_string(now->tm_mday) + "_" + std::to_string(now->tm_hour) + ":" + std::to_string(now->tm_min) + ":" + std::to_string(now->tm_sec) + " - ";
+      message += error;
+      o << message << std::endl;
+      }
+  };
--- ScreenshotReciever/src/log.hpp
+++ ScreenshotReciever/src/log.hpp
@@ -5,17 +5,19 @@
 
 class Log
 {
+private:
+  std::ofstream o;
 public:
-  void LogF(std::string error)
+  Log() : o("ScreenshotRecieverLog.txt")
+    {
+
+    }
+  void LogF(const std::string& error)
   {
-    std::ifstream i("log.txt");
-    std::string str((std::istreambuf_iterator<char>(i)), std::istreambuf_iterator<char>());
-    i.close();
     time_t t = time(0);
     struct tm * now = localtime(&t);
-    std::string name = std::to_string(now->tm_mday) + "_" + std::to_string(now->tm_hour) + ":" + std::to_string(now->tm_min) + ":" + std::to_string(now->tm_sec) + " - " + error + "\n";
-    std::ofstream o("log.txt");
-    o << str + name;
-    o.close();
+    std::string message = std::to_string(now->tm_mday) + "_" + std::to_string(now->tm_hour) + ":" + std::to_string(now->tm_min) + ":" + std::to_string(now->tm_sec) + " - ";
+    message += error;
+    o << message << std::endl;
   }
 };
